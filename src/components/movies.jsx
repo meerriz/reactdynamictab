9b426e9b6f5133b23002538ddab915d926b9b612
meerriz <talpur.rizwan@gmail.com>
2020-10-05 21:33:13 +0500

    deploy my dynamic table

---
+++ src/components/movies.jsx
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+import { getMovies } from "../services/fakeMovieService";
+
+class Movies extends Component {
+  state = {
+    movies: getMovies(),
+  };
+
+  handleDelete = (movie) => {
+    const movies = this.state.movies.filter((m) => m._id !== movie._id);
+    this.setState({ movies });
+  };
+
+  render() {
+    if (this.state.movies.length === 0)
+      return <p> There are no movies available for rent.</p>;
+    return (
+      <React.Fragment>
+        <p> Showing {this.state.movies.length} movies avaialable for rent.</p>
+        <table className="table table">
+          <thead className="thead-dark ">
+            <tr>
+              <th>Title</th>
+              <th>Genere</th>
+              <th>Stock</th>
+              <th>Rent</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.movies.map((movie) => (
+              <tr key={movie._id}>
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <button
+                    onClick={() => this.handleDelete(movie)}
+                    className="btn btn-danger btn-sm"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Movies;
